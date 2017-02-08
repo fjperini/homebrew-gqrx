ba6a36ecb3addb03ef1120e1c1dc04c7602da8e2
TT <edy555@gmail.com>
2017-02-08 13:32:12 +0900

    add gr-iqbal formula

---
+++ gr-iqbal.rb
@@ -0,0 +1,134 @@
+require 'formula'
+
+class GrIqbal < Formula
+  homepage 'http://sdr.osmocom.org/trac/wiki/GrOsmoSDR'
+  head 'git://git.osmocom.org/gr-iqbal'
+
+  depends_on 'cmake' => :build
+  depends_on 'gnuradio'
+
+  patch :DATA
+
+  def install
+    mkdir 'build' do
+      args = std_cmake_args
+      args << "-DENABLE_FCD=OFF" if build.without? 'fcd'
+      args << "-DPYTHON_LIBRARY=#{python_path}/Frameworks/Python.framework/"
+      system 'cmake', '..', *args
+      system 'make'
+      system 'make install'
+    end
+  end
+
+  def python_path
+    python = Formula.factory('python')
+    kegs = python.rack.children.reject { |p| p.basename.to_s == '.DS_Store' }
+    kegs.find { |p| Keg.new(p).linked? } || kegs.last
+  end
+end
+
+__END__
+diff --git a/libosmo-dsp/include/osmocom/dsp/cfile.h b/libosmo-dsp/include/osmocom/dsp/cfile.h
+index be9392e..eab9996 100644
+--- a/libosmo-dsp/include/osmocom/dsp/cfile.h
++++ b/libosmo-dsp/include/osmocom/dsp/cfile.h
+@@ -33,7 +33,7 @@
+  *  \brief Osmocom .cfile helpers header
+  */
+ 
+-#include <complex.h>
++#include "/usr/include/complex.h"
+ 
+ /*! \brief Structure representing a currently mapped .cfile */
+ struct cfile {
+diff --git a/libosmo-dsp/include/osmocom/dsp/cxvec.h b/libosmo-dsp/include/osmocom/dsp/cxvec.h
+index 063e30c..5adc771 100644
+--- a/libosmo-dsp/include/osmocom/dsp/cxvec.h
++++ b/libosmo-dsp/include/osmocom/dsp/cxvec.h
+@@ -33,7 +33,7 @@
+  *  \brief Osmocom Complex vectors header
+  */
+ 
+-#include <complex.h>
++#include "/usr/include/complex.h"
+ 
+ #define CXVEC_FLG_REAL_ONLY	(1<<0)	/*!< \brief Real values only */
+ 
+diff --git a/libosmo-dsp/include/osmocom/dsp/cxvec_math.h b/libosmo-dsp/include/osmocom/dsp/cxvec_math.h
+index 7e372c4..fa045b7 100644
+--- a/libosmo-dsp/include/osmocom/dsp/cxvec_math.h
++++ b/libosmo-dsp/include/osmocom/dsp/cxvec_math.h
+@@ -34,7 +34,7 @@
+  *  \brief Osmocom Complex vectors math header
+  */
+ 
+-#include <complex.h>
++#include "/usr/include/complex.h"
+ #include <math.h>
+ 
+ #include <osmocom/dsp/cxvec.h>
+diff --git a/libosmo-dsp/include/osmocom/dsp/iqbal.h b/libosmo-dsp/include/osmocom/dsp/iqbal.h
+index c7f7e60..5b2b037 100644
+--- a/libosmo-dsp/include/osmocom/dsp/iqbal.h
++++ b/libosmo-dsp/include/osmocom/dsp/iqbal.h
+@@ -33,7 +33,7 @@
+  *  \brief Osmocom IQ balance utils header
+  */
+ 
+-#include <complex.h>
++#include "/usr/include/complex.h"
+ 
+ #include <osmocom/dsp/cxvec.h>
+ 
+diff --git a/libosmo-dsp/src/cfile.c b/libosmo-dsp/src/cfile.c
+index 4fa927f..beae845 100644
+--- a/libosmo-dsp/src/cfile.c
++++ b/libosmo-dsp/src/cfile.c
+@@ -30,7 +30,7 @@
+  *  \brief Osmocom .cfile helpers implementation
+  */
+ 
+-#include <complex.h>
++#include "/usr/include/complex.h"
+ #include <fcntl.h>
+ #include <stdio.h>
+ #include <stdlib.h>
+diff --git a/libosmo-dsp/src/cxvec.c b/libosmo-dsp/src/cxvec.c
+index fcda8af..f6f0a9b 100644
+--- a/libosmo-dsp/src/cxvec.c
++++ b/libosmo-dsp/src/cxvec.c
+@@ -30,7 +30,7 @@
+  *  \brief Osmocom Complex vectors implementation
+  */
+ 
+-#include <complex.h>
++#include "/usr/include/complex.h"
+ #include <math.h>
+ #include <stdio.h>
+ #include <stdlib.h>
+diff --git a/libosmo-dsp/src/cxvec_math.c b/libosmo-dsp/src/cxvec_math.c
+index 64e2d1b..24ecb58 100644
+--- a/libosmo-dsp/src/cxvec_math.c
++++ b/libosmo-dsp/src/cxvec_math.c
+@@ -30,7 +30,7 @@
+  *  \brief Osmocom Complex vectors math implementation
+  */
+ 
+-#include <complex.h>
++#include "/usr/include/complex.h"
+ #include <math.h>
+ #include <stdio.h>
+ #include <stdlib.h>
+diff --git a/libosmo-dsp/src/iqbal.c b/libosmo-dsp/src/iqbal.c
+index e5432f9..f62db05 100644
+--- a/libosmo-dsp/src/iqbal.c
++++ b/libosmo-dsp/src/iqbal.c
+@@ -40,7 +40,7 @@
+  *   - Optimization based on steepest gradient with dynamic step size
+  */
+ 
+-#include <complex.h>
++#include "/usr/include/complex.h"
+ #include <stdlib.h>
+ #include <string.h>
+ 
