c7ff2f48d820c6e530d97dc7496cd9a76ee951a4
TT <edy555@gmail.com>
2018-04-08 13:11:49 +0900

    update formula for gqrx version 2.11.2

--- README.md
+++ README.md
@@ -7,24 +7,11 @@ that makes it easier get GQRX from source running on OS X.
 
 These steps have been tested on macSierra (10.12).
 
-- Install the python package prerequisites
-
-  ```sh
-  brew install python gfortran umfpack swig
-  ```
-
-- Install the prerequisite python packages
-
-  ```sh
-  pip install numpy scipy matplotlib Cheetah lxml 
-  ```
-
 - Install gqrx
 
   ```sh
   brew tap ttrftech/homebrew-gqrx
-  brew install gr-iqbal 
-  brew install gr-osmosdr [--with-rtlsdr] [--with-bladerf] [--with-airspy] [--with-hackrf]
   brew install gqrx 
   ```
 
+This formula installs gnuradio and gr-osmosdr module from tap ttrftech/gnuradio.
--- gqrx.rb
+++ gqrx.rb
@@ -2,17 +2,17 @@ require 'formula'
 
 class Gqrx < Formula
   homepage 'https://github.com/csete/gqrx'
-  url 'https://github.com/csete/gqrx/archive/v2.6.tar.gz'
-  sha256 '53d25db8d987a41ccccaf6fd85262bd7770cdfab5539b5901c4558756483c9db'
+  url 'https://github.com/csete/gqrx/archive/v2.11.2.tar.gz'
+  sha256 '28e2caea283027d58719e6ea8211462694d25816a37a510303d51af45decfcb3'
   head 'https://github.com/csete/gqrx.git'
 
   depends_on 'cmake'
   depends_on 'pkg-config'
   depends_on 'qt5'
   depends_on 'boost'
-  depends_on 'gnuradio'
-  depends_on 'gr-osmosdr'
-  depends_on 'gr-iqbal'
+  depends_on 'ttrftech/gnuradio/gnuradio'
+  depends_on 'ttrftech/gnuradio/gr-osmosdr'
+  depends_on 'ttrftech/gnuradio/gr-iqbal'
 
   def install
     args = "PREFIX=#{prefix}"
